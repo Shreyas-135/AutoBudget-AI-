09413823204edd8d06c680afe57931ebad3309bc
Shreyas S <shreyass0806@gmail.com>
2025-08-04 09:14:18 +0000

    The dashboard seems to be empty .The main content of dashboard could be

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -91,9 +91,11 @@ export default function DashboardLayout({
             </SidebarMenu>
           </SidebarFooter>
         </Sidebar>
-        <div className="flex flex-col sm:pl-14">
+        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
           <DashboardHeader />
-          <main className="flex-1">{children}</main>
+          <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 overflow-auto">
+            {children}
+          </main>
         </div>
       </div>
     </SidebarProvider>
